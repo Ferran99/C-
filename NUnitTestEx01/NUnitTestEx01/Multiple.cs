7ad4e8268b0a4e79540ad54ff17122e046b3f56a
Ferran Castañé <ferrancastane11@gmail.com>
2019-11-06 19:18:51 +0100

    Exercici Test

---
+++ NUnitTestEx01/NUnitTestEx01/Multiple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestEx01
+{
+    class Multiple
+    {
+        public static ArrayList Multiples(int i)
+        {
+            ArrayList multiples = new ArrayList();
+            for(int num = 0; num <= i; num++)
+            {
+                if(num % 3 == 0 && num % 5 == 0)
+                {
+                    multiples.Add("BooomPaaaam");
+                }else if(num % 3 == 0)
+                {
+                    multiples.Add("Booom");
+                }else if (num % 5 == 0)
+                {
+                    multiples.Add("Paaaam");
+                }
+                else
+                {
+                    multiples.Add(num.ToString());
+                }
+            }
+            return multiples;
+        }
+    }
+}
